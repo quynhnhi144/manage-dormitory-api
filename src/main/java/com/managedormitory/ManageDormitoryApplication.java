13eb3ac30440423d6a64a0e37200c08143fe41b7
quynhnhi144 <ltqnhi97@gmail.com>
2020-10-16 17:16:07 +0700

    initial project

---
+++ src/main/java/com/managedormitory/ManageDormitoryApplication.java
@@ -0,0 +1,13 @@
+package com.managedormitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDormitoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageDormitoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/managedormitory/ManageDormitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managedormitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageDormitoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
